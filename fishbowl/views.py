7b8f270db0ff9926ca2bbf325246a088fc280ef6
Harpaul Gill <harpaul_gill@hotmail.com>
2019-03-30 19:45:33 -0700

    updated so tcp received full msg

--- fishbowl/views.py
+++ fishbowl/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from django.http import JsonResponse
 from itertools import islice
+import struct
 
 # twitter stuff
 import os
@@ -235,8 +236,7 @@ def stream(request):
     s.listen(True)
     conn, addr = s.accept()
 
-    data = recvall(conn)
-    print(data)
+    data = recv_msg(conn)
     s.close()
     json_data = json.dumps(str(data))
     return JsonResponse(json_data, safe=False)
@@ -248,14 +248,22 @@ def tweet(msg, img):
     twitter.update_status(status=msg, media_ids=media_id)
     print("Tweeted: %s" % msg)
 
-def recvall(sock):
-    BUFF_SIZE = 4096 # 4 KiB
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
+
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
     data = b''
-    while True:
-        part = sock.recv(BUFF_SIZE)
-        data += part
-        if len(part) < BUFF_SIZE:
-            # either 0 or end of data
-            break
-    return data    
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data   
 
